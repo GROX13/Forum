f7368cfef150cf4b5a767724c8be146d25fd3fa2
GROX13 <giorgirokhadze@gmail.com>
2014-06-14 02:10:10 +0400

    Created test class

---
+++ Tests/test/managers/database/DataBaseManagerTest.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package test.managers.database;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Giorgi
+ * 
+ */
+public class DataBaseManagerTest {
+
+	/**
+	 * Test method for
+	 * {@link forum.managers.database.DataBaseManager#DataBaseManager(java.lang.String)}
+	 * .
+	 */
+	@Test
+	public void testDataBaseManager() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link forum.managers.database.DataBaseManager#executeSelect(java.lang.String)}
+	 * .
+	 */
+	@Test
+	public void testExecuteSelect() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link forum.managers.database.DataBaseManager#executeSelectWhere(java.lang.String, java.util.ArrayList, java.util.ArrayList, java.util.ArrayList)}
+	 * .
+	 */
+	@Test
+	public void testExecuteSelectWhere() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link forum.managers.database.DataBaseManager#executeOrderedSelect(java.lang.String, java.util.ArrayList, java.util.ArrayList, java.util.ArrayList, java.lang.String, int, int, boolean)}
+	 * .
+	 */
+	@Test
+	public void testExecuteOrderedSelect() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link forum.managers.database.DataBaseManager#executeInsert(java.lang.String, java.util.ArrayList, java.util.ArrayList)}
+	 * .
+	 */
+	@Test
+	public void testExecuteInsert() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link forum.managers.database.DataBaseManager#executeUpdate(java.lang.String, java.util.ArrayList, java.util.ArrayList, java.util.ArrayList, java.util.ArrayList)}
+	 * .
+	 */
+	@Test
+	public void testExecuteUpdate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link forum.managers.database.DataBaseManager#executeRemove(java.lang.String, java.util.ArrayList, java.util.ArrayList, java.util.ArrayList)}
+	 * .
+	 */
+	@Test
+	public void testExecuteRemove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link forum.managers.database.DataBaseManager#executeAdministration(java.lang.String)}
+	 * .
+	 */
+	@Test
+	public void testExecuteAdministration() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
