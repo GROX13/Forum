4a5cedeaf76696ef11575426eadb1671de05818d
AnnaGorozia <ana.gorozia@gmail.com>
2014-06-16 17:34:13 +0400

    TestAccountManager

--- Tests/test/managers/objects/TestAccountManager.java
+++ Tests/test/managers/objects/TestAccountManager.java
@@ -3,31 +3,37 @@ package test.managers.objects;
 import static org.junit.Assert.*;
 
 import java.sql.Date;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
+import forum.info.DataBaseInfo;
+import forum.managers.database.DataBaseManager;
 import forum.managers.objects.AccountManager;
 
 public class TestAccountManager {
 
 	@Test
-	public void testContainsAccount() {
-		
-		
+	public void testContainsAccount() throws SQLException {
+		am = new AccountManager();
+		assertEquals(true, am.containsAccount("GROX13"));
+		assertEquals(false, am.containsAccount("BLA"));
 	}
 
 	@Test
-	public void testMatchesPassword() {
-		
-	}
-
-	@Test
-	public void testCreateAccount() throws SQLException {
-		AccountManager am = new AccountManager();
+	public void testMatchesPassword() throws SQLException {
+		am = new AccountManager();
 		am.createAccount("ANNA", "bla", null, 
 				"anna", "gorozia", "ana.gorozia@gmail.com", 
 				"signature", "F", new Date(System.currentTimeMillis()), 1);
+		
+		assertEquals(false, am.matchesPassword("ANNA", "bla1"));
+		assertEquals(true, am.matchesPassword("ANNA", "bla"));
+		assertEquals(true, am.matchesPassword("GROX13", "asdasd"));
+	
 	}
-
+	
+	private AccountManager am ;
 }
