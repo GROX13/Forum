d7d010043b22659508b0becc7c056c686e2e1ab2
GROX13 <giorgirokhadze@gmail.com>
2014-06-21 16:19:25 +0400

    Chat

--- Servlet API dependent layer/servlet/chatroom/HandleChat.java
+++ Servlet API dependent layer/servlet/chatroom/HandleChat.java
@@ -39,7 +39,7 @@ public class HandleChat extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("me viyavi aq");
+		
 	}
 
 }
